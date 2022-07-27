e7487ea472fb25e332d26ff3d759f6dd773c004e
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2022-07-27 18:08:07 +0700

    Update bai tap 5.c

--- assignment 3/bai tap 5.c
+++ assignment 3/bai tap 5.c
@@ -14,6 +14,9 @@ int main() {
 	if((b-1)%2==0){
 		int x=((b-1)/2)*31+((b-1)/2)*30-c+a;
 		printf("ngay thu %d\n", x);
+		if(x%7==0)
+		printf("chu nhat");
+		else
 		printf("thu %d", (x%7)+1);
 	} else {
 		int x=(((b-2)/2)+1)*31+((b-2)/2)*30-c+a;
