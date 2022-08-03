7f972cd72e6d331657898bceed01d06f8bcde97b
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2022-08-03 20:36:16 +0700

    Create assignment7bai5.c

---
+++ assignment 7/assignment7bai5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	printf("Nhap so n = ");
+	scanf("%d", &n);
+	int a[n];
+	int i;
+	for(i=0;i<n;i++){
+		printf("Nhap so a[%d] = ", i);
+		scanf("%d", &a[i]);
+		int k;
+		for(k=0;k<i;k++){
+			for(;a[i]==a[k];){
+				printf("Nhap lai a[%d]: ",i);
+				scanf("%d", &a[i]);
+			}
+		}
+	}
+	printf("Ban da nhap dung");
+}
